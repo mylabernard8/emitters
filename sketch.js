39e1e78a6393f9c0164fb39d24c51a0d0c2dec75
mylabernard8 <myla.bernard26@avenues.org>
2025-05-13 23:30:30 -0400

    Added random number of initial particles to emitters

--- sketch.js
+++ sketch.js
@@ -1,48 +1,45 @@
-let emitters = []
-let G
+let emitters = [];
+let G;
 
 function setup() {
   createCanvas(400, 600);
-
-  emitters.push( new Emitter( width/2, 30 ) )
-  
-  G = createVector(0,0.1)
-  ellipseMode(RADIUS)
-  noStroke()
+  emitters.push(new Emitter(width / 2, 30));
+  G = createVector(0, 0.1);
+  ellipseMode(RADIUS);
+  noStroke();
 }
 
 function draw() {
   background(220);
-  for( let e of emitters ) {
-    e.update()  
-  }
-}
-
-class Emitter {
-  constructor( x, y ) {
-    this.x = x
-    this.y = y
-    this.particles = []
-    for( let i = 0; i < 20; i++ ) {
-      this.particles.push( new Particle(this.x, this.y) )
-    }    
-  }
-  
-  update() {
-    this.particles = this.particles.filter( p => !p.isDead() )
-
-    // draw all the live ones
-    for( let p of this.particles ) {
-      p.applyForce(G)
-      p.update()
-      p.draw()    
-    }
-
-    // add new ones
-    this.particles.push( new Particle(this.x, this.y) )     
+  for (let e of emitters) {
+    e.update();
   }
 }
 
 function mouseClicked() {
-  emitters.push( new Emitter(mouseX,mouseY) )
+  emitters.push(new Emitter(mouseX, mouseY));
+}
+
+class Emitter {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.particles = [];
+    let initialCount = int(random(10, 40)); // Random initial particle count
+    for (let i = 0; i < initialCount; i++) {
+      this.particles.push(new Particle(this.x, this.y));
+    }
+  }
+
+  update() {
+    this.particles = this.particles.filter((p) => !p.isDead());
+
+    for (let p of this.particles) {
+      p.applyForce(G);
+      p.update();
+      p.draw();
+    }
+
+    this.particles.push(new Particle(this.x, this.y));
+  }
 }
