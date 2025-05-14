8470b36d11b0118b8226944a7c27af2e7f7d611a
mylabernard8 <myla.bernard26@avenues.org>
2025-05-13 23:49:20 -0400

    Added image and edited code

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
 let emitters = [];
 let G;
+let particleImg;
+
+function preload() {
+  // Load an image to be used for ImageParticle
+  particleImg = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Yellow_star.svg/32px-Yellow_star.svg.png');
+}
 
 function setup() {
   createCanvas(400, 600);
@@ -27,8 +33,15 @@ class Emitter {
     this.particles = [];
     let initialCount = int(random(10, 40)); // Random initial particle count
     for (let i = 0; i < initialCount; i++) {
-      this.particles.push(new Particle(this.x, this.y));
+      this.particles.push(this.createRandomParticle(this.x, this.y));
     }
+  }
+
+  createRandomParticle(x, y) {
+    let type = random(["circle", "square", "image"]);
+    if (type === "square") return new SquareParticle(x, y);
+    else if (type === "image") return new ImageParticle(x, y);
+    else return new Particle(x, y);
   }
 
   update() {
@@ -40,6 +53,7 @@ class Emitter {
       p.draw();
     }
 
-    this.particles.push(new Particle(this.x, this.y));
+    this.particles.push(this.createRandomParticle(this.x, this.y));
   }
 }
+
