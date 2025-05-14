a6fae79d1d6cd816813aa9e792d71163916220a8
mylabernard8 <myla.bernard26@avenues.org>
2025-05-13 23:26:35 -0400

    Added in particle class to have random colors for each particle

--- Particle.js
+++ Particle.js
@@ -1,41 +1,39 @@
 class Particle {
-  constructor(x,y) {
-    this.position = createVector(x,y)
-    this.velocity = p5.Vector.random2D()
-    this.acceleration = p5.Vector.random2D()
-    this.r = 10
-    this.lifespan = 255
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.acceleration = p5.Vector.random2D();
+    this.r = 10;
+    this.lifespan = 255;
+    this.color = color(random(255), random(255), random(255)); // Random color
   }
-  
+
   update() {
-    this.velocity.add(this.acceleration)
-    this.position.add(this.velocity)   
-    this.acceleration.mult(0)
-    this.lifespan -= 2
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+    this.lifespan -= 2;
   }
-  
+
   isDead() {
-    return this.lifespan < 0
-    // if( this.lifespan < 0 ) {
-    //   return true 
-    // }
-    // return false
+    return this.lifespan < 0;
   }
-  
+
   draw() {
-    fill(255, this.lifespan)
-    circle(this.position.x,this.position.y,this.r)
+    fill(
+      this.color.levels[0],
+      this.color.levels[1],
+      this.color.levels[2],
+      this.lifespan
+    );
+    circle(this.position.x, this.position.y, this.r);
   }
-  
-  // f is a p5.Vector
+
   applyForce(f) {
-    this.acceleration.add(f)
+    this.acceleration.add(f);
   }
-  
-  
-  static createStandardParticleAt(x,y) {
-    return new Particle(
-      x,y
-    )
+
+  static createStandardParticleAt(x, y) {
+    return new Particle(x, y);
   }
 }
